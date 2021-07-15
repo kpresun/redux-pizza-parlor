9c21003ce8650370b5d15da703e2fdd9154f1b3c
Duy Dao <jimmydao@gmail.com>
2021-07-15 14:21:52 -0500

    Added a pizzaReducer, store, middleWare, and Provider for ReactDom.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+
+const pizzaReducer = (state = [], action) => {
+    return state;
+};
+
+// The store is the big JavaScript Object that holds all of the information for our application
+const storeInstance = createStore(
+  combineReducers({
+    
+  }),
+  applyMiddleware(logger)
+);
+
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
