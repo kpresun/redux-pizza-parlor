0d2ba1fd15226444c1f5b6e811e3f47e2602ce00
Kong Yang <kpyang2414@gmail.com>
2021-07-15 14:31:33 -0500

    committing new branch

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import {logger} from 'redux-logger';
 
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
