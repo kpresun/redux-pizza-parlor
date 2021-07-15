8f740d433a6dc8506b2c7fd9b56f75aa0c2d32f8
Matthew Bouc <boucgm@gmail.com>
2021-07-15 16:53:22 -0500

    checkout page in process - table building out

---
+++ src/components/CheckoutPage/CheckoutPage.jsx
@@ -0,0 +1,25 @@
+function CheckoutPage() {
+
+    return(
+        <section>
+            <h2>Step 3: Checkout</h2>
+            // import customer data here:  Name, Address, delivery/pickup
+
+            <table>
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Cost</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+
+            </table>
+        </section>
+    )
+}
+
+
+export default CheckoutPage
