4c740357493b73c64dfb53248de6434426d721f5
Sagethh <tate.feige@gmail.com>
2021-07-16 11:44:23 -0500

    added styling

--- src/components/PizzaItem/PizzaItem.jsx
+++ src/components/PizzaItem/PizzaItem.jsx
@@ -53,9 +53,11 @@ function PizzaItem({ pizza }) {
         </CardMedia>
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            {pizza.name} {pizza.price}
+            {pizza.name} 
+            <br>
+            </br>${pizza.price}
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p" style={{height: 125}}>
+          <Typography variant="body2" color="textSecondary" component="p" style={{height: 95}}>
             {pizza.description}
           </Typography>
         </CardContent>
