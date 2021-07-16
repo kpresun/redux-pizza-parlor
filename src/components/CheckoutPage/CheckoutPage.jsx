466dbaa3322058cf9c09abdd11d441447f2febd0
Matthew Bouc <boucgm@gmail.com>
2021-07-16 10:20:48 -0500

    null

--- src/components/CheckoutPage/CheckoutPage.jsx
+++ src/components/CheckoutPage/CheckoutPage.jsx
@@ -4,12 +4,21 @@ import CheckoutList from "../CheckoutList/CheckoutList";
 function CheckoutPage() {
 
     const handleCheckoutClick = () => {
-        axios.post('')
+        axios.post('/api/router', ) // need to post multiple things - customer, pizza list
+        .then(response => {
+            console.log('success POSTing', response);
+            //navigate back to "homepage"
+            //clear out the necessary reducers (checkout reducer, customer reducer)
+
+        })
+        .catch(error => {
+            console.log('error POSTing', error);
+        })
     }
     return(
         <section>
             <h2>Step 3: Checkout</h2>
-            // import customer data here:  Name, Address, delivery/pickup
+            {/* import customer data here:  Name, Address, delivery/pickup */}
 
             <table>
                 <thead>
