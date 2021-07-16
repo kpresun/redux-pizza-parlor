e20c0b3d9629de47a273e4a50c23555bee7eb449
Duy Dao <jimmydao@gmail.com>
2021-07-15 19:38:07 -0500

    Re-pushing customer form because it looks like the master pull formatted over my current file.

--- src/components/CustomerForm/CustomerForm.jsx
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -1,3 +1,47 @@
-import Axios from 'axios';
+import axios from 'axios';
 import {useState, useEffect} from 'react';
+
+function CustomerForm ({refreshCustomer}) {
+
+    const [customerName, setCustomerName] = useState();
+
+    useEffect(() => {
+    console.log(customerName);
+    }, []);
+
+    const handleSubmit = event => {
+
+        axios.post('/api/order',{customerName})
+        .then(response =>{
+        console.log(`What is the POST response`,response.data);
+        refreshCustomer();
+        setCustomerName('');
+        })
+        .catch(error => {
+            console.log('Error in POST. Please try again.', error);
+        });
+    }
+
+    return (
+        <form className="addCustomer" onSubmit={handleSubmit}>
+            <input value={customerName} placeholder="Name" 
+                onChange={(event) => 
+                setCustomerName(event.target.customer_name)}/>
+            <input value={customerName} placeholder="Street Address" 
+                onChange={(event) => 
+                setCustomerName(event.target.street_address)}/>
+            <input value={customerName} placeholder="City" 
+                onChange={(event) => 
+                setCustomerName(event.target.city)}/>
+            <input value={customerName} placeholder="Zip Code" 
+                onChange={(event) => 
+                setCustomerName(event.target.zip)}/>
+            <button type="radio">Pickup</button>
+            <button>Delivery</button>
+            <button type="submit">Next</button>
+        </form>
+    )
+}
+
+export default CustomerForm;
 
